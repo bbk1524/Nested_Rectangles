92afa627e3ebb8b73a43839940c14bc7819701da
Ben Kane's Win Tower <bbk1524@gmail.com>
2015-07-05 22:53:47 -0500

    Modified trim_libraries. About to get rid of liquidfun and classify everythin

--- Libraries/trim_libraries.py
+++ Libraries/trim_libraries.py
@@ -9,7 +9,7 @@ import os
 import fnmatch
 
 # Things to keep
-file_endings = (".dat", ".xml", ".ini", ".md", ".cmake", ".py")
+file_endings = (".dat", ".xml", ".ini", ".md", ".cmake", ".py", ".mk")
 library_endings = (".dll", ".a", ".lib", ".dylib", ".so")
 code_endings = (".c", ".C", ".c++", ".cc", ".cxx", ".cpp")
 header_endings = (".h", ".H", ".h++", ".hh", ".hxx", ".hpp")
@@ -34,7 +34,11 @@ def print_files(root_dir):
     for root, dirs, files in os.walk(root_dir):
         for name in files:
             if not wanted_file(name):
-                print(os.path.join(root, name).replace(root_dir, '.'))
+                # get name local to this file by deleting the root dir part
+                name = os.path.join(root, name).replace(root_dir, '')[1:]
+                #replace backslashes with forward slashes
+                name = name.replace('\\', '/')
+                print(name)
     print("@"*15)
 
 def delete_files(root_dir):
@@ -44,18 +48,33 @@ def delete_files(root_dir):
         for name in files:
             if not wanted_file(name):
                 print(os.path.join(root, name).replace(root_dir, '.'))
-                file.write(os.path.join(root, name).replace(root_dir, '.') + '\n')
+                file.write(os.path.join(root, name).replace(root_dir, '') + '\n')
                 os.remove(os.path.join(root, name))
                 #pass
     file.close()
-				
+	
+def gitignore_files(root_dir):
+    print("Deleting ... ")
+    file = open(".gitignore", 'a')
+    file.write("# The following files will be ignored:\n")
+    for root, dirs, files in os.walk(root_dir):
+        for name in files:
+            if not wanted_file(name):
+                # get name local to this file by deleting the full path
+                name = os.path.join(root, name).replace(root_dir, '')[1:]
+                #replace backslashes with forward slashes
+                name = name.replace('\\', '/')
+                print(name)
+                file.write(name + '\n')
+    file.close()
+			
 # Use location of script as root directory
 root_dir = os.path.dirname(os.path.realpath(__file__))
 
 def make_choice():
     text = """
     Root directory: {}
-    About to delete all files not ending in:
+    About to add to .gitignore all files not ending in:
         {}
         {}
         {}
@@ -65,7 +84,7 @@ def make_choice():
     or names matching the pattern of:
         {}
     Root directory: {}
-    Type 'c' to see files that will be deleted.
+    Type 'c' to see files that will be added.
     Continue [y/c/n]?
     """.strip().format(root_dir, file_endings, library_endings, code_endings, 
                         header_endings, file_names, file_patterns, root_dir)
@@ -79,6 +98,6 @@ def main():
         print_files(root_dir)
         choice = make_choice()
     if choice == 'y':
-        delete_files(root_dir)
+        gitignore_files(root_dir)
         
 main()        
